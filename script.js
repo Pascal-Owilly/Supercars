c12a461f29f4bd141bff00c5525f9ca371ba2bcb
Owilly <pasaclouma54@gmail.com>
2021-11-30 18:51:37 +0300

    Add lambo

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ var circle = document.getElementById("circle");
 var upBtn = document.getElementById("upBtn");
 var downBtn = document.getElementById("downBtn");
 
-
 var rotateValue = circle.style.transform;
 upBtn.onclick = function() {
    rotateSum = rotateValue + "rotate(-90deg)";
